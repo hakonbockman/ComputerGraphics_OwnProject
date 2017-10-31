c8eb777c1f0b5e753253bd36d628240216144068
hakonbockman <hakonbockman@gmail.com>
2017-10-31 11:48:17 +0100

    Added multiple lights, watch out the colours are really "fabulous" at this moment.

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -37,7 +37,11 @@ public class MainGameLoop {
 		MasterRenderer renderer = new MasterRenderer();
 		GuiRenderer guiRenderer = new GuiRenderer(loader);
 	// Light
-		Light light = new Light(new Vector3f(3000, 2000, 500), new Vector3f(1, 1, 1));
+		Light light = new Light(new Vector3f(0, 10000, -7000), new Vector3f(1, 1, 1));
+		List<Light> lights = new ArrayList<Light>();
+		lights.add(light);
+		lights.add(new Light(new Vector3f(-200, 10, -200), new Vector3f(10, 0, 0)));
+		lights.add(new Light(new Vector3f(200, 10, 200), new Vector3f(10, 0, 0)));
 	// Light end	
 	// Player
 		TexturedModel personModel = loadTexturedModel("models/person", "textures/playerTexture", 1, loader);		
@@ -116,7 +120,7 @@ public class MainGameLoop {
 			renderTerrain(renderer, terrains);
 			renderEntities(renderer, entities);
 			
-			renderer.render(light, camera);
+			renderer.render(lights, camera);
 			guiRenderer.render(guis);
 			
 			DisplayManager.updateDisplay();
--- src/shaders/TerrainShader.java
+++ src/shaders/TerrainShader.java
@@ -1,5 +1,7 @@
 package shaders;
 
+import java.util.List;
+
 import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector3f;
 
@@ -9,14 +11,16 @@ import toolbox.Maths;
 
 public class TerrainShader extends shaderProgram{
 	
+	private static final int MAX_LIGHTS = 4;
+	
 	private static final String VERTEX_FILE = "src/shaders/TerrainVertexShader.txt";
 	private static final String FRAGMENT_FILE = "src/shaders/TerrainFragmentShader.txt";
 	
 	private int location_transformationMatrix;
 	private int location_projectionMatrix;
 	private int location_viewMatrix;
-	private int location_lightPosition;
-	private int location_lightColour;
+	private int location_lightPosition[];
+	private int location_lightColour[];
 	private int location_shineDamper;
 	private int location_reflectivity;
 	private int location_skycolour;
@@ -44,8 +48,6 @@ public class TerrainShader extends shaderProgram{
 		location_transformationMatrix = super.getUniformLocation("transformationMatrix");
 		location_projectionMatrix = super.getUniformLocation("projectionMatrix");
 		location_viewMatrix = super.getUniformLocation("viewMatrix");
-		location_lightPosition = super.getUniformLocation("lightPosition");
-		location_lightColour = super.getUniformLocation("lightColour");
 		location_shineDamper = super.getUniformLocation("shineDamper");
 		location_reflectivity = super.getUniformLocation("reflectivity");
 		location_skycolour = super.getUniformLocation("skyColour");
@@ -55,6 +57,12 @@ public class TerrainShader extends shaderProgram{
 		location_bTexture = super.getUniformLocation("bTexture");
 		location_blendMap = super.getUniformLocation("blendMap");
 		
+		location_lightPosition = new int[MAX_LIGHTS];
+		location_lightColour = new int [MAX_LIGHTS];
+		for(int i = 0; i < 4; i++) {
+			location_lightPosition[i] = super.getUniformLocation("lightPosition[" + i + "]");
+			location_lightColour[i] = super.getUniformLocation("lightColour[" + i + "]");
+		}
 	}
 	
 	public void connectTextureUnits() {
@@ -78,9 +86,16 @@ public class TerrainShader extends shaderProgram{
 		super.loadMatrix(location_transformationMatrix, matrix);
 	}
 	
-	public void loadLight(Light light) {
-		super.loadVector(location_lightPosition, light.getPosition());
-		super.loadVector(location_lightColour, light.getColour());
+	public void loadLights(List<Light> lights) {
+		for(int i = 0; i < 4; i++) {
+			if(i < lights.size()) {
+				super.loadVector(location_lightPosition[i], lights.get(i).getPosition());
+				super.loadVector(location_lightColour[i], lights.get(i).getColour());
+			}else {
+				super.loadVector(location_lightPosition[i], new Vector3f(0, 0, 0));
+				super.loadVector(location_lightColour[i],  new Vector3f(0, 0, 0));
+			}
+		}
 	}
 	
 	public void loadProjectionMatrix(Matrix4f projection) {
